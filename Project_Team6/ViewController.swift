d72e2c86f0cf0aa343abd7cea1362225e1600271
Team6 <team6@administrators-mac-mini-3.local>
2018-10-08 11:13:03 -0500

    Initial Commit

---
+++ Project_Team6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_Team6
+//
+//  Created by Team6 on 10/8/18.
+//  Copyright © 2018 Team6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
